d4ac52b5a2fb464260742b6e8518b849f04e701b
fengweijie123 <310424554@qq.com>
2021-07-14 17:01:17 +0800

    Commit Message

---
+++ src/main/java/com/cykj/HelloControler.java
@@ -0,0 +1,25 @@
+package com.cykj;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/admin")
+public class HelloControler {
+
+
+    @RequestMapping("/hello")
+    public String Hello(){
+        return "hello";
+    }
+
+    @ResponseBody
+    @PostMapping("/getHello4")
+    public  String getHello4(String username,String password){
+        System.out.println(username);
+        System.out.println(password);
+        return "1111";
+    }
+}
